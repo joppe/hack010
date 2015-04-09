712f2aa19b151cb2d529f1ba86e0886ddabb4a2f
Joppe Aarts <joppe@apestaartje.info>
2015-04-09 15:08:47 +0200

    dev

--- js/presentation/view/map.js
+++ js/presentation/view/map.js
@@ -8,12 +8,30 @@
 (function (z, $) {
     'use strict';
 
+    var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(51.921019, 4.479429)
+        };
+
     /**
      * @namespace presentation.view
      */
     z.createNamespace('presentation.view');
 
     presentation.view.Map = presentation.view.Abstract.extend({
+        /**
+         * @param {Object} options
+         */
+        initialize: function (options) {
+            presentation.view.Abstract.prototype.initialize.call(this, options);
 
+            this.markers = {
+                locations: [],
+                guides: [],
+                users: []
+            };
+
+            this.map = new google.maps.Map(this.el, mapOptions);
+        }
     });
 }(zicht, jQuery));
